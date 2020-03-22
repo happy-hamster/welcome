97385e821409ce1030e9a9121a4adf55067fb884
sdnyco <sidney.francois@gmail.com>
2020-03-22 01:23:32 +0100

    dont look at this effing mess but it almost works

---
+++ app.js
@@ -0,0 +1,65 @@
+let animationConfig = {
+  interval: 8000 // ms
+}
+
+let statusConfig = {
+  yes: {
+    class: 'yes',
+    text: 'Go',
+    title: 'Almost Full'
+  },
+  no: {
+    class: 'no',
+    text: 'No',
+    title: 'All Clear'
+  }
+}
+
+// set initial status
+let isFull = true
+let isInitialRun = true
+
+let updateStatusBadge = function(selectedStatus) {
+  let badge = $('.status-badge')
+  let node = $('.animation-inner')
+
+  badge.remove()
+
+  node.append('<div class="animated status-badge bounce ' + statusConfig[selectedStatus].class + '">' + statusConfig[selectedStatus].text + '</div>')
+
+  isFull = !isFull
+}
+
+let updateStatusCapacity = function() {
+  if (isFull) {
+    let node = $('.status-capacity')
+
+    $('.icon-person.delay-2s').addClass('hide')
+    $('.icon-person.delay-3s').addClass('hide')
+
+    $('.icon-person.delay-2s').remove()
+    $('.icon-person.delay-3s').remove()
+
+    node.append('<img class="icon-person animated delay-1s bounceIn hide" src="img/icon_person_outline.svg" />')
+    node.append('<img class="icon-person animated delay-2s bounceIn hide" src="img/icon_person_outline.svg" />')
+
+    $('.icon-person.delay-1s').removeClass('hide')
+    $('.icon-person.delay-2s').removeClass('hide')
+
+    isFull = false
+    isInitialRun = false
+
+    $('.status-title').text('Empty')
+
+    updateStatusBadge('yes')
+  }
+}
+
+$(document).on('ready', function() {
+
+  if (isInitialRun) {
+    setTimeout(function() {
+      updateStatusCapacity()
+    }, 5000)
+  }
+})
